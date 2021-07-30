9b3ee3587c5a8744ae6fb0be81adec30f391c265
Domen Hočevar <domenhocevar1@gmail.com>
2021-07-30 18:25:46 +0200

    Removed lines placed by accident

--- routes/catalog.js
+++ routes/catalog.js
@@ -9,10 +9,5 @@ const tagController = require('../controllers/tagController');
 
 router.get("/taglist", tagController.tagList);
 
-router.get("/:id", tagController.tagDetail);
-
-router.get("/:id/detail", function(res, req) {
-    res.redirect('..')
-});
 
 module.exports = router;
