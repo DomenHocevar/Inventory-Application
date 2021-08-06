24e64acb75b2e6510d7117926791a6bf575e1059
domen <domenhocevar1@gmail.com>
2021-08-06 11:20:10 +0200

    Added navigation bar and wrote the home/index page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Inventory Application'});
 });
+
+router.get('/home', function(req, res) {
+  res.redirect('./');
+})
 
 module.exports = router;
